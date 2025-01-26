3ee1d64654fc9a7cf4d9e471f10adbf3c8ccce26
Sean Woo <sean.woo@mail.utoronto.ca>
2025-01-26 06:05:45 -0500

    extractor completed

--- Classes/Patient.py
+++ Classes/Patient.py
@@ -1,6 +1,5 @@
 class Patient:
-    def __init__(self, p_id, arrival_time, queue_position, status, time_elapsed,
-                 triage_category):
+    def __init__(self, p_id, arrival_time, queue_position, status, time_elapsed, triage_category):
         self.id = p_id
         self.arrival_time = arrival_time
         self.queue_position = queue_position
@@ -9,7 +8,6 @@ class Patient:
         self.triage_category = triage_category
 
     def __repr__(self):
-        """Return a string representation of the patient."""
         return (f"Patient(id={self.id}, arrival_time={self.arrival_time}, "
                 f"queue_position={self.queue_position}, status={self.status}, "
                 f"time_elapsed={self.time_elapsed}, triage_category={self.triage_category})")
--- extractor.py
+++ extractor.py
@@ -1,12 +1,38 @@
+import requests
 import pandas as pd
-import json
+from Classes.Patient import Patient
 
-with open('test_data', 'r') as file:
-    data = json.load(file)
+url = "https://ifem-award-mchacks-2025.onrender.com/api/v1/queue"
 
-longest_wait_time = data['longestWaitTime']
-waiting_count = data['waitingCount']
+def fetch_data():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
 
-patients = data['patients']
-df = pd.DataFrame(patients)
-df.to_csv('patients_data.csv', index=False)
+def extract_data(data):
+    if data is None:
+        return None
+
+    patients = data["patients"]
+    longest_wait_time = data["longestWaitTime"]
+    waiting_time = data["waitingCount"]
+    patient_data = []
+    for patient in patients:
+        p = Patient(patient["id"],
+                    patient["arrival_time"],
+                    patient["queue_position"],
+                    patient["status"],
+                    patient["time_elapsed"],
+                    patient["triage_category"])
+        patient_data.append(p)
+
+    return patient_data, longest_wait_time, waiting_time
+
+data = fetch_data()
+d = extract_data(data)
+
